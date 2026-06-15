1e89b6d47105a4db9e888ed8fe323f54a379db59
babski86 <xuucca9330@gmail.com>
2026-06-15 20:08:44 +0400

    html is cvlilebebi sectionebshi

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,19 @@
-// ვპოულობთ სურათების "მატარებელ" ზოლს
 const track = document.getElementById("slider-track");
 
-// სულ რამდენი სურათი გვაქვს შიგნით
 const totalImages = track ? track.children.length : 0;
 let currentIndex = 0;
 
 function slideImages() {
   if (!track) return;
 
-  // გადავდივართ შემდეგ სურათზე
   currentIndex = (currentIndex + 1) % totalImages;
 
-  // ვამოძრავებთ ლენტას მარცხნივ: 0%, -100%, -200%
   track.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
-
-// 5 წამში ერთხელ გადააჩოჩოს
 setInterval(slideImages, 5000);
+// const menu = document.getElementById("mobile-menu");
+// const toggleBtn = document.getElementById("menu-toggle");
+// toggleBtn.addEventListener("click", () => {
+//   menu.classList.toggle("right-0");
+//   menu.classList.toggle("right-[-80%]");
+// });
