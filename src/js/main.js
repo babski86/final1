f131a0c82feb47891d6d72d7536ba80ba419c0b3
babski86 <xuucca9330@gmail.com>
2026-06-16 19:48:48 +0400

    vtestav telefonis versias

--- src/js/main.js
+++ src/js/main.js
@@ -11,9 +11,67 @@ function slideImages() {
   track.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
 setInterval(slideImages, 5000);
-// const menu = document.getElementById("mobile-menu");
-// const toggleBtn = document.getElementById("menu-toggle");
-// toggleBtn.addEventListener("click", () => {
-//   menu.classList.toggle("right-0");
-//   menu.classList.toggle("right-[-80%]");
+const mobileMenu = document.getElementById("mobile-menu");
+const menuToggle = document.getElementById("menu-toggle");
+const overlay = document.getElementById("overlay");
+
+const menuLinks = mobileMenu.querySelectorAll("a");
+
+function openMenu() {
+  mobileMenu.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+function closeMenu() {
+  mobileMenu.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+menuToggle.addEventListener("click", (event) => {
+  event.stopPropagation();
+
+  if (mobileMenu.classList.contains("hidden")) {
+    openMenu();
+  } else {
+    closeMenu();
+  }
+});
+
+// overlay-ზე დაკლიკებით დახურვა
+overlay.addEventListener("click", closeMenu);
+
+// ლინკზე დაკლიკებით დახურვა
+menuLinks.forEach((link) => {
+  link.addEventListener("click", closeMenu);
+});
+const section = document.getElementById("scroll-div");
+const progressBars = document.querySelectorAll("[data-progress]");
+const burtula = document.querySelector("#burtula");
+const sectionWidth = section.offsetWidth;
+const gorgola = document.querySelectorAll("[data-gorgola]");
+const observer = new IntersectionObserver(
+  (entries) => {
+    if (entries[0].isIntersecting) {
+      progressBars.forEach((e, i) => {
+        const progress = e.dataset.progress;
+        e.style.width = `${progress}%`;
+
+        const left = sectionWidth * (progress / 100) - 12;
+        const currentGorgola = gorgola[i];
+
+        if (currentGorgola) {
+          // 1. აღარ გვჭირდება className-ის წაშლა და კლასების დამატება
+          // 2. პირდაპირ ვუცვლით marginLeft-ს:
+          currentGorgola.style.marginLeft = `${left}px`;
+        }
+      });
+      console.log("სექციის 30% ჩანს");
+    }
+  },
+  { threshold: 0.3 },
+);
+// gorgola.forEach((e) => {
+//   e.classList.add("ml-[200px]");
 // });
+console.log(gorgola);
+observer.observe(section);
